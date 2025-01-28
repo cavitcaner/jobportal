68cde6d503140fbfc3c42be7126f6fb847a00fe8
cavitcaner <50204510+cavitcaner@users.noreply.github.com>
2025-01-28 13:59:53 +0300

    Update IBenefitService.cs

--- APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/IBenefitService.cs
+++ APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/IBenefitService.cs
@@ -3,8 +3,20 @@ namespace JobPortal.JobPostingService.Application.Interfaces
 {
     public interface IBenefitService
     {
+        /// <summary>
+        /// yeni bir Yan hak oluştururak db'ye kayıt yapar
+        /// </summary>
+        /// <returns></returns>
         Task CreateBenefitAsync(Benefit benefit, CancellationToken cancellationToken);
+        /// <summary>
+        /// Belirli bir yan hak bilgisini db'den çekip döner.
+        /// </summary>
+        /// <returns></returns>
         Task<Benefit> GetBenefitByIdAsync(Guid id, CancellationToken cancellationToken);
+        /// <summary>
+        /// Yan haklar listesini db'den alıp döner.
+        /// </summary>
+        /// <returns></returns>
         Task<ICollection<Benefit>> GetAllBenefitsAsync(CancellationToken cancellationToken);
     }
 }
