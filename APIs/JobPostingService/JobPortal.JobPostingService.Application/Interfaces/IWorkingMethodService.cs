12a554920c8dcf5567dd7c27bbdb0029912d603f
cavitcaner <50204510+cavitcaner@users.noreply.github.com>
2025-01-28 14:07:12 +0300

    Update IWorkingMethodService.cs

--- APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/IWorkingMethodService.cs
+++ APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/IWorkingMethodService.cs
@@ -4,8 +4,20 @@ namespace JobPortal.JobPostingService.Application.Interfaces
 {
     public interface IWorkingMethodService
     {
+        /// <summary>
+        /// Veritabanına yeni bir çalışma türü ekler
+        /// </summary>
+        /// <returns></returns>
         Task CreateWorkingMethodAsync(WorkingMethod workingMethod, CancellationToken cancellationToken);
+        /// <summary>
+        /// Veritabanından belirli bir çaışma tipini döner
+        /// </summary>
+        /// <returns></returns>
         Task<WorkingMethod> GetWorkingMethodByIdAsync(Guid id, CancellationToken cancellationToken);
+        /// <summary>
+        /// Veritabanından bütün çalışma türü listesini döner
+        /// </summary>
+        /// <returns></returns>
         Task<ICollection<WorkingMethod>> GetAllWorkingMethodsAsync(CancellationToken cancellationToken);
     }
 } 
