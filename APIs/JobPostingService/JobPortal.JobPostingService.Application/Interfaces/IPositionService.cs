c870b4ee5b4fa031dc3559733c2bfb8a975a561b
cavitcaner <50204510+cavitcaner@users.noreply.github.com>
2025-01-28 14:06:26 +0300

    Update IPositionService.cs

--- APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/IPositionService.cs
+++ APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/IPositionService.cs
@@ -4,8 +4,20 @@ namespace JobPortal.JobPostingService.Application.Interfaces
 {
     public interface IPositionService
     {
+        /// <summary>
+        /// Veritabanına yeni bir pozisyon ekler
+        /// </summary>
+        /// <returns></returns>
         Task CreatePositionAsync(Position position, CancellationToken cancellationToken);
+        /// <summary>
+        /// Veritabanından belirli bir pozisyon bilgisini döner
+        /// </summary>
+        /// <returns></returns>
         Task<Position> GetPositionByIdAsync(Guid id, CancellationToken cancellationToken);
+        /// <summary>
+        /// Veritabanından bütün pozisyon listesini döner
+        /// </summary>
+        /// <returns></returns>
         Task<ICollection<Position>> GetAllPositionsAsync(CancellationToken cancellationToken);
     }
 } 
