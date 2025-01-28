a0d06fbbd724cf66c8f8f3045768c58e573757ac
cavitcaner <50204510+cavitcaner@users.noreply.github.com>
2025-01-28 13:58:26 +0300

    Update IEmployerEventService.cs

--- APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/EventServices/IEmployerEventService.cs
+++ APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/EventServices/IEmployerEventService.cs
@@ -8,10 +8,23 @@ using System.Threading.Tasks;
 
 namespace JobPortal.JobPostingService.Application.Interfaces.EventServices
 {
-
+    
+    /// <summary>
+    /// Kuyrukla ilgili metodları bulundurur
+    /// </summary>
     public interface IEmployerEventService
     {
+         /// <summary>
+        /// Bir işverenin ilan yayınlama limitini ve şirket adını dönen event metodudur
+        /// </summary>
+        /// <param name="employerId">İşveren Id</param>
+        /// <returns>Şirket adı ve ilan yayınlama limitini döner</returns>
         Task<GetEmployerJobPostingLimitEventResponse> GetEmployerJobPostingLimit(Guid employerId);
+        
+        /// <summary>
+        /// Bir ilan oluşturulduğunda ilanı oluşturan işverenin limitini düşürmek için bir event fırlatır.
+        /// </summary>
+        /// <returns></returns>
         Task SendJobPostCreatedEventAsync(JobPostCreatedEventRequest body);
     }
 }
