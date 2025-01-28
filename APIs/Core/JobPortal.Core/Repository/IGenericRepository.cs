480bbf028eb249b022b4c2e3f0327185b9f8dd11
cavitcaner <50204510+cavitcaner@users.noreply.github.com>
2025-01-28 14:33:14 +0300

    Update IGenericRepository.cs

--- APIs/Core/JobPortal.Core/Repository/IGenericRepository.cs
+++ APIs/Core/JobPortal.Core/Repository/IGenericRepository.cs
@@ -10,13 +10,37 @@ namespace JobPortal.Core.Repository
 {
     public interface IGenericRepository<T> where T : BaseEntity, new()
     {
+        /// <summary>
+        /// Veritabanında query'e göre arama yapar
+        /// </summary>
         Task<ICollection<T>> GetWithQueryAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken);
+        /// <summary>
+        /// Veritabanında query'e göre kayıt var mı yok mu bilgisini döner
+        /// </summary>
         Task<bool> AnyWithQueryAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken);
+        /// <summary>
+        /// Belirli bir id'ye göre veritabanından kayıt döner
+        /// </summary>
         Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        /// <summary>
+        /// İlgili tipteki bütün kayıtları döner
+        /// </summary>
         Task<ICollection<T>> GetAllAsync(CancellationToken cancellationToken);
+        /// <summary>
+        /// İlgili tipte yeni kayıt oluşturur.
+        /// </summary>
         Task AddAsync(T entity, CancellationToken cancellationToken);
+        /// <summary>
+        /// Belirtilen Db'deki bir kaydı günceller
+        /// </summary>
         Task UpdateAsync(T entity, CancellationToken cancellationToken);
+        /// <summary>
+        /// Belirtilen Id'ye ait Db'deki bir kaydı siler
+        /// </summary>
         Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+        /// <summary>
+        /// Belirtilen Db'deki bir kaydı siler
+        /// </summary>
         Task DeleteAsync(T entity, CancellationToken cancellationToken);
     }
 }
