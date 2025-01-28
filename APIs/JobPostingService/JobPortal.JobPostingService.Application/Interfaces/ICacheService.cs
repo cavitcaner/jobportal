fdd4598774a533b0db4bb0fc2dc06cf79872d699
cavitcaner <50204510+cavitcaner@users.noreply.github.com>
2025-01-28 14:01:42 +0300

    Update ICacheService.cs

--- APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/ICacheService.cs
+++ APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/ICacheService.cs
@@ -2,9 +2,25 @@
 {
     public interface ICacheService<T>
     {
+        /// <summary>
+        /// Cache'den belirilten keye göre data çekip döner.
+        /// </summary>
+        /// <returns></returns>
         Task<T?> GetAsync(string key);
+        /// <summary>
+        /// Cache'den belirilten keye göre dataları çekip döner. liste halinde atılan cache dataları için kullanılır
+        /// </summary>
+        /// <returns></returns>
         Task<List<T>?> GetListAsync(string key);
+        /// <summary>
+        /// Cache'den belirilten key'e ait dataları siler
+        /// </summary>
+        /// <returns></returns>
         Task RemoveAsync(string key);
+        /// <summary>
+        /// Cache'e belirtilen key'de ve expration tarihine göre kayıt ekler.
+        /// </summary>
+        /// <returns></returns>
         Task SetAsync(string key, List<T> value, TimeSpan expiration);
 
     }
