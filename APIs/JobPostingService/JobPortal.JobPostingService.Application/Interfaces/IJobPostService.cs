2d13629b50de770cb9a2be7aacd02b79668a2063
cavitcaner <50204510+cavitcaner@users.noreply.github.com>
2025-01-28 14:02:58 +0300

    Update IJobPostService.cs

--- APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/IJobPostService.cs
+++ APIs/JobPostingService/JobPortal.JobPostingService.Application/Interfaces/IJobPostService.cs
@@ -4,8 +4,20 @@ namespace JobPortal.JobPostingService.Application.Interfaces
 {
     public interface IJobPostService
     {
+        /// <summary>
+        /// Veritabanına yeni bir ilan eklemek için kullanılır.
+        /// </summary>
+        /// <returns></returns>
         Task CreateJobPostAsync(JobPost jobPost, CancellationToken cancellationToken);
+        /// <summary>
+        /// Veritabanından belirli bir ilan bilgilerini döner. 
+        /// </summary>
+        /// <returns></returns>
         Task<JobPost> GetJobPostByIdAsync(Guid id, CancellationToken cancellationToken);
+        /// <summary>
+        /// Veritabanında olan büün ilanları listeler.
+        /// </summary>
+        /// <returns></returns>
         Task<ICollection<JobPost>> GetAllJobPostsAsync(CancellationToken cancellationToken);
     }
 } 
